ae24ec38e8a9b619771785c595d5e79d58eb736d
sunzhiwen <Sunsunsunvip@163.com>
2022-11-18 16:29:31 +0800

    测试代码

--- js/index1.js
+++ js/index1.js
@@ -1,9 +1,10 @@
 $(function () {
-	const getData = async function () {
-		const data = await axios.post("http://39.185.228.30:15108/release//manage/group/define/list", {
-			"page": 1,
-		})
-		console.log(data);
-	}
-	getData()
+	// const getData = async function () {
+	// 	const data = await axios.post("http://39.185.228.30:15108/release//manage/group/define/list", {
+	// 		"page": 1,
+	// 	})
+	// 	console.log(data);
+	// }
+	// getData()
+	console.log(1111);
 })
