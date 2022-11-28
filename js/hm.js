8c7f2af07a30602e4f2e64f520674168bf970d66
sunzhiwen <Sunsunsunvip@163.com>
2022-11-28 18:09:15 +0800

    加入引入文件

---
+++ js/hm.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?02eed5b0e271910672850a4da9b0ddd2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
